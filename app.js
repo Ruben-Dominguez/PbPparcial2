95bef2bd85f539d45b9b87b07833feddbb0c252f
Ruben Dominguez <a01568047@gmail.com>
2021-05-15 14:49:54 -0600

    Se agrega el index.html, se agrega la posibilidad de leer el archivo y otras rutas al server

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const fs = require('fs');
 const http = require('http');
 
+// server creado en el puerto 8888
 http.createServer((request, response) =>{
-  const file = request.url;
+  // interpolacion de la ruta dada. default -> index.html
+  const file = request.url == "/" ? "./index.html" : `./${request.url}`;
   fs.readFile(file, (error, data)=>{
-    response.writeHead(200, {"Content-Type":"text/plain"});
-    response.write("Hola Mundo!");
-    response.end();
+    if(error) {
+      response.writeHead(404, {"Content-Type":"text/plain"});
+      response.write("Not found :(");
+      response.end();
+    }
+    else {
+      response.writeHead(200, {"Content-Type":"text/html"});
+      response.write(data);
+      response.end();
+    }
   });
 
 }).listen(8888);
