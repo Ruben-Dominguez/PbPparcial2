b7b889c19ac8aa16f78b4ff2f9802925dabc0b1a
Juan Pablo <a329688@uach.mx>
2021-05-19 21:27:20 -0600

    finish html file and js store/retieve data

--- contacto/script.js
+++ contacto/script.js
@@ -1,16 +1,19 @@
-const fs = require('fs');
-
-const addContacto = () => {
-    let fName = document.getElementById("fName");
-    let lName = document.getElementById("lName");
-    let phone = document.getElementById("phone");
-    let email = document.getElementById("email");
+function addContacto(){
+    let fName = document.getElementById("fName").value;
+    let lName = document.getElementById("lName").value;
+    let phone = document.getElementById("phone").value;
+    let email = document.getElementById("email").value;
     data = `${fName},${lName},${phone},${email}\n`;
-    
-    fs.appendFile('contact.txt', data.toString());
-    fName.innerHTML = "";
-    lName.innerHTML = "";
-    phone.innerHTML = "";
-    email.innerHTML = "";
+    localStorage.setItem(email,data);
+    document.getElementById("fName").innerHTML = "";
+    document.getElementById("lName").innerHTML = "";
+    document.getElementById("phone").innerHTML = "";
+    document.getElementById("email").innerHTML = "";
 }
 
+function showData(){
+  for (i = 0; i < localStorage.length; i++) {
+    x = localStorage.getItem(localStorage.key(i));
+    console.log(x);
+  }
+}
